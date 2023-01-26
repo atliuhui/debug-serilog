ab34551a651aed7221cc5cc951798ce113665187
Hui LIU <atliuhui@outlook.com>
2023-01-26 11:35:44 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
